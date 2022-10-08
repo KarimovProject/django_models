8aea4fe24ff49015c007069f27224ce7da93cb4f
KarimovProject <abdullatifkarimov772@gmail.com>
2022-10-08 09:42:51 +0500

    models

--- databaseapp/models.py
+++ databaseapp/models.py
@@ -1,5 +1,12 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create model called User
 # It's have to have these fields
 # first_name, last_name, email, phone_number
+
+class User_person(models.Model):
+    first_name = models.TextField()
+    last_name = models.TextField()
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=40)
