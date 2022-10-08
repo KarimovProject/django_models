cd7cdb186b9652a024364bddc38f396290027dde
KarimovProject <abdullatifkarimov772@gmail.com>
2022-10-08 09:43:12 +0500

    views.py

--- databaseapp/views.py
+++ databaseapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import User_person
 # Create your views here.
+def first_names(x):
+    first_names = User_person.objects.all()
+    return first_names[0].first_name
